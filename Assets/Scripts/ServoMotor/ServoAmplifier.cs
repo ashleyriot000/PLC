bc86861b50da39502686e4bb8fc24425a8661019
Ashley <ashleyriot000@gmail.com>
2026-01-08 07:59:17 +0900

    서보 앰프 구현중

--- Assets/Scripts/ServoMotor/ServoAmplifier.cs
+++ Assets/Scripts/ServoMotor/ServoAmplifier.cs
@@ -1,22 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Events;
+using static ServoActuator;
 
 [RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(ConfigurableJoint))]
 public class ServoAmplifier : MonoBehaviour
 {
     #region Constants
+    //연결된 액츄에이터의 구동 방식
     public enum ActuatorType
     {
         Linear,
         Rotary
     }
+    //액츄에이터를 제어할 때 사용할 단위
     public enum UnitType
     {
-        MM,
-        Degree,
-        Pulse
+        MM,         //밀리미터
+        Degree,     //각도
+        Pulse       //펄스값
     }
+    //서보 동작 상태
     public enum ServoState
     {
         Off = 0,
@@ -25,8 +32,20 @@ public class ServoAmplifier : MonoBehaviour
         Positioning,
         Homing_Search,
         Homing_Retry,
-        Homing_Creep,
-        Error
+        Homing_Creep
+    }
+
+    public enum OperationPattern
+    {
+        End = 0,
+        Continuos,
+        Location
+    }
+
+    public enum ControlMethodType
+    {
+        ABS_Linear1 = 0,
+        INC_Linear1
     }
 
     // 상수 정의 (1mm = 1000 pulse/um)
@@ -34,26 +53,51 @@ public class ServoAmplifier : MonoBehaviour
     const double UNITY_TO_PLC_RATIO = 1000000.0d;
     #endregion
 
-    #region Variables
-    public Rigidbody body;
-    public ConfigurableJoint joint;
+    #region Custom Type
+    [Serializable]
+    public struct PositioningData
+    {
+        public OperationPattern pattern;
+        public ControlMethodType controlMethod;
+        public double accelTime;
+        public double decelTime;
+        public double posAddress;
+        public double commandSpeed;
+        public double dwellTime;
+    }
+    #endregion
 
+    #region Variables
+    public Rigidbody body;          //물리 강체
+    public ConfigurableJoint joint; //로컬 조인트
+
+    [Header("액츄에이터 동작 방식")]
     public ActuatorType actuatorType = ActuatorType.Linear;
-    public UnitType usedUnit;
-    public bool useForceStop = false;
-    public double motorResolution = 131072d;
-    public double gearRatio = 1.0d;
-    public double ballscrewLead = 2d;        //milimeter
-    public double maxSpeed = 2000.0d;     //milimeter/min
-    public double accelTime = 1000d;        //milisecond
-    public double decelTime = 1000d;        //milisecond
-    public int defaultHomingDirection = 1;
-    public double jogSpeed = 1000d;
-    public double homingHighSpeed = 200.0d;
-    public double homingCreepSpeed = 20.0d;
-    public double inPosWidth = 0.1d;
-    public double inPositionDuration = 0.1d;
-    
+    [Header("Basic Parameter")]    
+    public UnitType usedUnit = UnitType.MM;     //사용 단위
+    public double motorResolution = 131072d;    //1회전당 펄스수(분해능)
+    public double ballscrewLead = 2d;           //1회전당 이동 길이(mm)
+    public double gearRatio = 1.0d;             //기어비
+    public double speedLimit = 2000.0d;         //최대 속도(mm/min)
+    public double accelTime = 1d;            //가속시간(초)
+    public double decelTime = 1d;            //감속시간(초)
+
+    [Header("Detail Parameter")]
+    public double inPosWidth = 0.01d;           //인-포지션 폭 (오차 허용 범위:mm)
+    public double jogSpeedLimit = 200d;         //JOG 기동 속도(mm/min)
+    public double jogAccelTime = 500d;          //JOG 가속시간 
+    public double jogdecelTime = 500d;          //JOG 감속시간
+    public bool useForceStop = false;           //강제 정지 허용 여부(신호를 계속 주고 있어야 정지하지 않음)
+    public int inPositionDuration = 100;    //목표 도달 신호 유지시간(millisecond)
+
+    [Header("HPR(원점 복귀) 파라미터")]
+    public int defaultHomingDirection = 1;      //원점복귀 기본 방향(1:정방향, -1:역방향)
+    public double homingHighSpeed = 200.0d;     //원점복귀 최대속도
+    public double homingCreepSpeed = 20.0d;     //원점복귀 정밀속도
+    public double homingAccelTime = 1d;         //원점복귀 가속시간
+    public double homingDecelTime = 1d;         //원점복귀 감속시간
+
+    public List<PositioningData> positioningList; //포지셔닝 데이터 리스트
 
     public UnityEvent<bool> onReadyChanged;
     public UnityEvent<bool> onErrorChanged;
@@ -74,12 +118,12 @@ public class ServoAmplifier : MonoBehaviour
     private bool _isBusy = false;
     private bool _isError = false;
 
+    private bool _isOnJogForward = false;
+    private bool _isOnJogReverse = false;
     private bool _isOnLimitSensorPositive = false;
     private bool _isOnLimitSensorNegative = false;
     private bool _isOnProximityDog = false;
     private bool _OPRComplete = false;
-    private bool _isOnJogForward = false;
-    private bool _isOnJogReverse = false;
     private bool _inPosition = false;
     private bool _isNotForceStop = false;
 
@@ -87,7 +131,7 @@ public class ServoAmplifier : MonoBehaviour
     private double _currentVelocity_Unit = 0d;
     private double _internalTarget_Unit = 0d;
     private double _targetUnit = 0d;
-    private int _homingDir = 1;
+    private int _currentHomingDir = 0;
 
 
     #endregion
@@ -124,6 +168,7 @@ public class ServoAmplifier : MonoBehaviour
         {
             if (_isBusy == value)
                 return;
+
             _isBusy = value;
             onBusyChanged?.Invoke(value);
         }
@@ -212,9 +257,11 @@ public class ServoAmplifier : MonoBehaviour
     #region UNITY EVENT METHOD
     private void Awake()
     {
+        //리지드 바디가 연결되어 있지 않으면 
         if(body == null)
-            body = GetComponent<Rigidbody>();
+            body = GetComponent<Rigidbody>(); //게임오브젝트 안에 어태치된 리지드바디를 찾아서 넣어라.
 
+        //리지드바디를 찾았다면
         if (body != null)
         {
             body.automaticCenterOfMass = false;
@@ -224,9 +271,11 @@ public class ServoAmplifier : MonoBehaviour
             body.useGravity = false;
         }
 
+        //컨피규러블 조인트가 연결되어 있지 않다면
         if(joint == null)
-            joint = GetComponent<ConfigurableJoint>();
+            joint = GetComponent<ConfigurableJoint>();  //게임오브젝트 안에 어태치된 조인트를 찾아서 넣어라
 
+        //조인트를 찾았다면
         if(joint != null)
         {
             joint.xMotion = ConfigurableJointMotion.Locked;
@@ -240,45 +289,19 @@ public class ServoAmplifier : MonoBehaviour
 
     private void FixedUpdate()
     {
+        double accTime;
         double targetVelocity = _currentState switch
         {
-            ServoState.Jogging => RunJogging(),
-            ServoState.Positioning => RunPositioning(),
+            ServoState.Jogging=> ProcessJog(out accTime),
+            ServoState.Positioning => ProcessPositioning(out accTime),
             ServoState.Homing_Search => throw new System.NotImplementedException(),
             ServoState.Homing_Retry => throw new System.NotImplementedException(),
             ServoState.Homing_Creep => throw new System.NotImplementedException(),
-            _  => 0d
-        };
+            _ => 0d
+        };             
 
-        bool isHoming =
-            (_currentState == ServoState.Homing_Search) ||
-            (_currentState == ServoState.Homing_Retry) ||
-            (_currentState == ServoState.Homing_Creep);
-
-        if(_isOnLimitSensorPositive && targetVelocity > 0d)
-        {
-            targetVelocity = 0d;
-            if(!isHoming)
-            {
-                IsError = true;
-                _currentState = ServoState.Error;
-                return;
-            }
-        }
-
-        if(_isOnLimitSensorNegative && targetVelocity < 0d)
-        {
-            targetVelocity = 0d;
-            if (!isHoming)
-            {
-                IsError = true;
-                _currentState = ServoState.Error;
-                return;
-            }
-        }
-
-        double referenceSpeed = _currentState == ServoState.Jogging ? jogSpeed : maxSpeed;
-        double accelRate = referenceSpeed / (accelTime * 0.001f);
+        double limitSpeed = _currentState == ServoState.Positioning ? speedLimit : jogSpeedLimit;
+        double accelRate = limitSpeed / accelTime;
         _currentVelocity_Unit = MoveTowards(_currentVelocity_Unit, targetVelocity, accelRate * Time.fixedDeltaTime);
 
         _internalTarget_Unit += _currentVelocity_Unit * Time.fixedDeltaTime;
@@ -290,7 +313,7 @@ public class ServoAmplifier : MonoBehaviour
 
     #endregion
 
-    #region Private Method
+    #region Private Method    
     private double SetJointForce(float force)
     {
         JointDrive drive = new JointDrive
@@ -311,7 +334,6 @@ public class ServoAmplifier : MonoBehaviour
 
         return 0d;
     }
-
     private double MoveTowards(double current, double target, double maxDelta)
     {
         // 1. 현재 값과 목표 값의 차이(절대값)가 최대 변화량보다 작거나 같으면
@@ -325,21 +347,6 @@ public class ServoAmplifier : MonoBehaviour
         //    Math.Sign: 목표가 더 크면 1, 작으면 -1 반환
         return current + Math.Sign(target - current) * maxDelta;
     }    
-
-    private double RunJogging()
-    {
-        if (_isOnJogForward)
-            return jogSpeed;
-        else if (_isOnJogReverse)
-            return -jogSpeed;
-
-        _currentState = ServoState.Idle;
-        return 0d;
-    }
-    private double RunPositioning()
-    {
-        return 0;
-    }
     private double PulseToUnit(int pulse)
     {
         double revs = (double)pulse / motorResolution;
@@ -390,18 +397,94 @@ public class ServoAmplifier : MonoBehaviour
             joint.targetRotation = Quaternion.Euler((float)degValue, 0, 0);
         }
     }
+    private async Task CompletePositioning()
+    {
+        InPosition = true;
 
+        if (inPositionDuration > 0)
+        {
+            try
+            {
+                await Task.Delay(inPositionDuration, destroyCancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                return;
+            }
 
+            InPosition = false;
+        }
+    }
+
+    private double ProcessJog(out double accelTime)
+    {
+        accelTime = 0d;
+        double direction = 0;
+        if (_isOnJogForward)
+        {
+            if (_isOnLimitSensorPositive)
+            {
+                _currentVelocity_Unit = 0d;
+                IsError = true;
+                return 0d;
+            }
+
+            accelTime = jogAccelTime;
+            direction = 1d;
+        }
+
+        if (_isOnJogReverse)
+        {
+            if (_isOnLimitSensorNegative)
+            {
+                _currentVelocity_Unit = 0d;
+                IsError = true;
+                return 0d;
+            }
+            direction += -1d;
+            accelTime += jogdecelTime;
+        }
+
+        return jogSpeedLimit * direction;
+    }
+    private double ProcessHomingSearch(out double accelTime)
+    {
+        accelTime = 0d;
+        return homingHighSpeed * _currentHomingDir;
+    }
+
+    private double ProcessPositioning(out double accelTime)
+    {
+        accelTime = 0d;
+        if(_isOnLimitSensorPositive || _isOnLimitSensorNegative)
+        {
+            _currentVelocity_Unit = 0d;
+            IsError = true;
+            return 0d;
+        }
+
+        double distance = _targetUnit - _internalTarget_Unit;
+        if(Math.Abs(distance) <= inPosWidth)
+        {
+            _currentVelocity_Unit = 0d;
+            _internalTarget_Unit = _targetUnit;
+            ApplyPhysics(_internalTarget_Unit);
+            return 0d;
+        }
+
+        return 1d;
+    }
     #endregion
 
     #region Public Method
-    public void SetServoOn(bool isOn)
+    public void CommandPLCReady(bool isOn)
     {
         if(isOn)
         {
             IsReady = true;
             _currentState = ServoState.Idle;
             _homeOffset_Unit = 0;
+            SetJointForce(100000f);
         }
         else
         {
@@ -411,7 +494,70 @@ public class ServoAmplifier : MonoBehaviour
             IsError = false;
             OPRComplete = false;
             _currentState = ServoState.Off;
+            SetJointForce(0f);
         }
-    }    
+    }
+
+    public void SetForceStop(bool isOn)
+    {
+        if (!useForceStop)
+            return;
+
+        //true: 일반
+        //false: 강제 정지상태
+    }
+
+
+    public void CommandJogForward(bool isOn)
+    {
+        if(_currentState == ServoState.Idle || 
+            _currentState == ServoState.Jogging)
+        {
+            _isOnJogForward = isOn;
+
+            _currentState = !_isOnJogForward && !_isOnJogReverse ?
+            ServoState.Idle : ServoState.Jogging;
+
+            IsBusy = _currentState == ServoState.Jogging;
+        }
+        else
+        {
+            IsError = true;
+        }
+    }
+    public void CommandJogReverse(bool isOn)
+    {
+        if(_currentState == ServoState.Idle ||
+            _currentState == ServoState.Jogging)
+        {
+            _isOnJogReverse = isOn;
+        }
+
+        _currentState = !_isOnJogForward && !_isOnJogReverse ?
+            ServoState.Idle : ServoState.Jogging;
+
+        IsBusy = _currentState == ServoState.Jogging;
+    }
+    public void CommandPositioning(int num)
+    {
+        if (IsError)
+            return;
+
+        if (_currentState != ServoState.Idle)
+        {
+            IsError = true;
+            return;
+        }
+
+        PositioningData data = positioningList[num - 1];
+        if (data.controlMethod == ControlMethodType.ABS_Linear1)
+            _targetUnit = data.posAddress;
+        else if (data.controlMethod == ControlMethodType.INC_Linear1)
+            _targetUnit += data.posAddress;
+    }
+
+
+
+
     #endregion
 }
